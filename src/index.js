ef46370a5b827ed8aed6eca026bafdf7b5e763e0
Joseph Noblett <noblettjoe@gmail.com>
2023-05-15 20:22:25 -0500

    added two evt listeners mouseover, mouseout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './less/index.less'
 
 // Your code goes here!
+const signUpBtn = Array.from(document.querySelectorAll(".btn"));
+const navLinks = document.querySelectorAll("a");
+const images = Array.from(document.images);
+const webpageText = Array.from(document.querySelectorAll("h1, a, h2, p, h3, h4"));
+const copyright = document.querySelector(".footer p");
+const destinations = Array.from(document.querySelectorAll(".destination"));
+const mainContent = document.querySelector(".home");
+const logo = document.querySelector(".logo-heading");
+
+
+const mouseoverEvt = (element, color1, color2) => {
+    element.style.color = color1;
+    element.style.background = color2;
+}
+
+const navLinksArr = Array.from(navLinks);
+
+navLinksArr.forEach((link) => {
+    link.addEventListener("mouseover", (evt) => {
+        mouseoverEvt(link, "yellow", "black");
+    })
+    link.addEventListener("mouseout", (evt) => {
+        mouseoverEvt(link, "black", "white");
+    })
+})
+
+
